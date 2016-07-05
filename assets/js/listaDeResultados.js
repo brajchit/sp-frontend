eb6e4c28a329c5cc210ec96569840c7c99c96ef9
Joyce <jebenite@espol.edu.ec>
2016-07-05 10:38:02 -0500

    modal de notificacion en listaDeResultadoshtml

--- assets/js/listaDeResultados.js
+++ assets/js/listaDeResultados.js
@@ -1,10 +1,4 @@
-
-	
-		
-		
-		
-		
-        var xhttp=new XMLHttpRequest();
+	var xhttp=new XMLHttpRequest();
 		xhttp.open("GET","assets/json/listaDeResultados.json",true);
 			xhttp.send();
 			
@@ -31,20 +25,6 @@
 					
 					
 					var men = document.createElement("a");
-					
-					
-						
-						
-						
-							
-							
-					
-						
-						
-							
-							
-						
-					
 					tr.appendChild(td1);
 					tr.appendChild(td2);
 					tr.appendChild(td3);
@@ -59,8 +39,27 @@
 							
 						}
 						else{
-							men.className="fa fa-envelope-o "
 							men.setAttribute("href", "#");
+							men.className="fa fa-envelope-o "
+							var modal = document.getElementById('myModal');
+							var span = document.getElementsByClassName("close")[0];
+							
+							men.addEventListener("click",function(){
+								modal.style.display = "block";
+							})
+								
+
+								// When the user clicks on <span> (x), close the modal
+								span.onclick = function() {
+									modal.style.display = "none";
+								}
+
+								// When the user clicks anywhere outside of the modal, close it
+								window.onclick = function(event) {
+									if (event.target == modal) {
+										modal.style.display = "none";
+									}
+								}
 							men.textContent="Notificar";
 							td4.appendChild(men);
 							
